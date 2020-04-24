54bfd14505c57c8838fcb96c440dbbfaefad38e4
Tim MacDonald <hello@timacdonald.me>
2020-04-24 10:53:40 +1000

    wip

---
+++ src/rules.php
@@ -0,0 +1,11 @@
+<?php
+
+// last reviewed: v2.16.3 Yellow Bird
+
+return [
+    '@PSR2' => true,
+    'array_syntax' => ['syntax' => 'short'],
+    'final_class' => false,
+    'new_with_braces' => true,
+    // ...
+];
